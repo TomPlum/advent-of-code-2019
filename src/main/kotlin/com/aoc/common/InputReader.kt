6cae2a35cdca05122c167f479a34bb3b613ee11d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2019-12-13 22:30:25 +0000

    Day 1 | Fixed InputReader Resource Issue

--- src/main/kotlin/com/aoc/common/InputReader.kt
+++ src/main/kotlin/com/aoc/common/InputReader.kt
@@ -4,9 +4,9 @@ import com.aoc.value.Day
 import java.io.File
 
 class InputReader {
-    fun readInputString(day: Day): Input<String> = Input(File(getPath(day)).readLines())
-    fun readInputInteger(day: Day): Input<Int> = Input(File(getPath(day)).readLines().map { it.toInt() })
-    fun readInputDouble(day: Day): Input<Double> = Input(File(getPath(day)).readLines().map { it.toDouble() })
+    fun readInputString(day: Day): Input<String> = Input(read(day))
+    fun readInputInteger(day: Day): Input<Int> = Input(read(day).map { it.toInt() })
+    fun readInputDouble(day: Day): Input<Double> = Input(read(day).map { it.toDouble() })
 
-    private fun getPath(day: Day) = "/day${day.value}/input.txt"
+    private fun read(day: Day) = javaClass.getResource("/day${day.value}/input.txt").path.lines()
 }

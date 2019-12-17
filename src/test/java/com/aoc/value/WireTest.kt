6a5687dc9e8917ccd69af368a586f11f047a125d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2019-12-17 21:00:21 +0000

    Day 3 | Added Wire Test

--- src/test/java/com/aoc/value/WireTest.kt
+++ src/test/java/com/aoc/value/WireTest.kt
@@ -10,7 +10,13 @@ internal class WireTest {
     @DisplayName("Given a valid input String, when creating a new Wire, then it should parse the Wire Segments correctly")
     internal fun createWireFromInput() {
         val wire = Wire("L1,R2,D4")
-        val segments = wire.segments
-        assertThat(segments).isEqualTo(listOf(WireSegment("L", 1), WireSegment("R", 2), WireSegment("D", 4)))
+        assertThat(wire.segments).isEqualTo(listOf(WireSegment("L", 1), WireSegment("R", 2), WireSegment("D", 4)))
+    }
+
+    @Test
+    @DisplayName("Given a valid input String that has lengths greater than 1 digit, when creating a new Wire, then it should parse the Wire Segments correctly")
+    internal fun createWireFromInputWithBiggerLengths() {
+        val wire = Wire("U123,D75634,L87")
+        assertThat(wire.segments).isEqualTo(listOf(WireSegment("U", 123), WireSegment("D", 75634), WireSegment("L", 87)))
     }
 }

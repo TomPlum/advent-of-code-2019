07083f52c383d522ae7f34159441cb45026daf30
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-02-18 19:46:07 +0000

    Day 8 | Added SpaceImageVerifier Test Suite (Includes Part 1 Solution Test)

---
+++ src/test/java/com/aoc/image/SpaceImageVerifierTest.kt
@@ -0,0 +1,22 @@
+package com.aoc.image
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.aoc.input.Day
+import com.aoc.input.InputReader
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+
+class SpaceImageVerifierTest {
+    @Test
+    @DisplayName("Given the SpaceImage generated from Day 8's Puzzle Input, when verifying the image is not corrupt," +
+    "then it should return 2904")
+    fun partOneSolution() {
+        val imageData = InputReader().readInputAsSingleString(Day.from(8))
+        val dimensions = SpaceImageDimensions(25, 6)
+        val image = ImageDataDecoder().decode(imageData, dimensions)
+        val solution = SpaceImageVerifier(image).verify(0, 1, 2)
+        assertThat(solution).isEqualTo(2904)
+    }
+}

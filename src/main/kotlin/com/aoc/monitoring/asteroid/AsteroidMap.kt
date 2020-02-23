f720a730b69a6ffb58d1eb8f253c1458b76a2abb
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-02-23 19:03:33 +0000

    Day 10 | Added Stack Overflow URL (Computing Vector Angles Programmatically)

--- src/main/kotlin/com/aoc/monitoring/asteroid/AsteroidMap.kt
+++ src/main/kotlin/com/aoc/monitoring/asteroid/AsteroidMap.kt
@@ -12,6 +12,7 @@ class AsteroidMap(mapData: List<String>) {
     fun getRow(index: Int) = map[index].joinToString(separator = "")
 
     fun getAsteroidSightLine(x1: Int, y1: Int, x2: Int, y2: Int) {
+        //https://stackoverflow.com/questions/14066933/direct-way-of-computing-clockwise-angle-between-2-vectors/16544330#16544330
         val dotProduct = x1 * x2 + y1 * y2
         val determinant = x1 * y2 - y1 * x2
         val angle = atan2(determinant.toDouble(), dotProduct.toDouble())

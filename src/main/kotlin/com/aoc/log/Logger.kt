61f512b64cacc7e4b18e27d371ab201360b536a0
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-03-07 20:25:15 +0000

    Logging | Added Logging Level Methods

--- src/main/kotlin/com/aoc/log/Logger.kt
+++ src/main/kotlin/com/aoc/log/Logger.kt
@@ -4,8 +4,19 @@ import org.slf4j.LoggerFactory.getLogger
 
 class Logger {
     companion object {
-        @Suppress("JAVA_CLASS_ON_COMPANION")
         @JvmStatic
+        @Suppress("JAVA_CLASS_ON_COMPANION")
         private val logger = getLogger(javaClass.enclosingClass)
+
+        fun debug(message: String) = logger.debug(message)
+
+        fun info(message: String) = logger.info(message)
+
+        fun warn(message: String) = logger.warn(message)
+
+        fun error(message: String) = logger.error(message)
+
+        fun trace(message: String) = logger.trace(message)
     }
+
 }
---
+++ src/test/java/com/aoc/log/LoggerTest.kt
@@ -0,0 +1,10 @@
+package com.aoc.log
+
+import org.junit.jupiter.api.Test
+
+class LoggerTest {
+     @Test
+     fun debug() {
+         Logger.debug("DEBUG LOG!")
+     }
+}

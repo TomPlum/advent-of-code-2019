fdd195b1c62e235693b66792653490d4322da6bf
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-03-20 19:15:47 +0000

    Day 16 | Part 1 - Implemented Signal Pattern Logic

--- implementation/src/main/kotlin/com/aoc/radio/Signal.kt
+++ implementation/src/main/kotlin/com/aoc/radio/Signal.kt
@@ -1,5 +1,16 @@
 package com.aoc.radio
 
+import java.lang.IllegalArgumentException
+
 data class Signal(val sequence: List<Int>) {
+    private val basePattern = listOf(0, 1, 0, -1)
+
+    fun getPattern(sequenceIndex: Int): List<Int> {
+        if (sequenceIndex > sequence.size - 1) {
+            throw IllegalArgumentException("Invalid Sequence Index ($sequenceIndex) for Signal Length ${sequence.size}")
+        }
+        return basePattern.flatMap { datum -> (0..sequenceIndex).map { datum } }
+    }
+
     override fun toString() = sequence.joinToString(separator = "")
 }
--- implementation/src/test/java/com/aoc/radio/SignalTest.kt
+++ implementation/src/test/java/com/aoc/radio/SignalTest.kt
@@ -3,10 +3,33 @@ package com.aoc.radio
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 class SignalTest {
     @Test
     fun toStringTest() {
         assertThat(Signal(listOf(1,2,3,4,5)).toString()).isEqualTo("12345")
+    }
+
+    @Test
+    fun getPatternBaseIndex() {
+        assertThat(Signal(listOf(1,2,3)).getPattern(0)).isEqualTo(listOf(0,1,0,-1))
+    }
+
+    @Test
+    fun getPatternSecondIndex() {
+        assertThat(Signal(listOf(1,2,3)).getPattern(1)).isEqualTo(listOf(0,0,1,1,0,0,-1,-1))
+    }
+
+    @Test
+    fun getPatternThirdIndex() {
+        assertThat(Signal(listOf(1,2,3)).getPattern(2)).isEqualTo(listOf(0,0,0,1,1,1,0,0,0,-1,-1,-1))
+    }
+
+    @Test
+    fun getPatternIndexOutOfBounds() {
+        val e = assertThrows<IllegalArgumentException> { Signal(listOf(1, 2, 3)).getPattern(4) }
+        assertThat(e.message).isEqualTo("Invalid Sequence Index (4) for Signal Length 3")
     }
 }

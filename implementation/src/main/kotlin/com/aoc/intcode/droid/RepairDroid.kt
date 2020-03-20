421cc276219e4f1f4415f46168961720a59c9e46
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-03-20 10:07:22 +0000

    Day 15 | RepairDroid Cleaning, Kotlin Doc & Reduced Redundancy

--- implementation/src/main/kotlin/com/aoc/intcode/droid/RepairDroid.kt
+++ implementation/src/main/kotlin/com/aoc/intcode/droid/RepairDroid.kt
@@ -6,10 +6,15 @@ import java.util.*
 
 class RepairDroid(instructions: String) {
     private val computer = IntCodeComputer(instructions)
-    private val shipFloorMap = ShipFloorMap()
-    private val visited = Stack<Pair<Point2D, Direction>>()
+    private val path = Stack<Pair<Point2D, Direction>>()
+    private val map = ShipFloorMap()
     private var x = 0
     private var y = 0
+
+    /**
+     * @return The current [ShipFloorMap] that the [RepairDroid] has mapped during its exploration.
+     */
+    fun downloadShipMappingData() = map
 
     /**
      * Command the [RepairDroid] to explore the ship in order to find the Oxygen System.
@@ -18,11 +23,11 @@ class RepairDroid(instructions: String) {
      */
     fun locateShipsOxygenSystem(): Pair<Point2D, Int> {
         //Set Starting Tile & Initial Movement Direction
-        visited.push(Pair(Point2D(0, 0), Direction.NORTH))
+        path.push(Pair(Point2D(0, 0), Direction.NORTH))
 
         var oxygenSystemInfo: Pair<Point2D, Int>? = null
 
-        while (!visited.isEmpty()) {
+        while (!path.isEmpty()) {
             val direction = getNextDirection()
 
             computer.getProgramMemory().input.add(direction.code.toLong())
@@ -31,73 +36,69 @@ class RepairDroid(instructions: String) {
             when (DroidResponse.fromCode(computer.getDiagnosticCode().toInt())) {
                 DroidResponse.HIT_WALL_POSITION_NOT_CHANGED -> {
                     //Record Wall Coordinate
+                    val wall = ShipFloorTile.WALL
                     when (direction) {
-                        Direction.NORTH -> shipFloorMap.updateTile(Point2D(x, y + 1), ShipFloorTile.WALL)
-                        Direction.EAST -> shipFloorMap.updateTile(Point2D(x + 1, y), ShipFloorTile.WALL)
-                        Direction.SOUTH -> shipFloorMap.updateTile(Point2D(x, y - 1), ShipFloorTile.WALL)
-                        Direction.WEST -> shipFloorMap.updateTile(Point2D(x - 1, y), ShipFloorTile.WALL)
+                        Direction.NORTH -> map.updateTile(Point2D(x, y + 1), wall)
+                        Direction.EAST -> map.updateTile(Point2D(x + 1, y), wall)
+                        Direction.SOUTH -> map.updateTile(Point2D(x, y - 1), wall)
+                        Direction.WEST -> map.updateTile(Point2D(x - 1, y), wall)
                     }
                 }
                 DroidResponse.SUCCESSFULLY_CHANGED_POSITION -> {
                     //Record Current Coordinate As Traversable (Unless Inside Oxygen System)
-                    if (shipFloorMap.tileType(Point2D(x, y)) != ShipFloorTile.OXYGEN_SYSTEM) {
-                        shipFloorMap.updateTile(Point2D(x, y), ShipFloorTile.TRAVERSABLE)
+                    if (map.tileType(Point2D(x, y)) != ShipFloorTile.OXYGEN_SYSTEM) {
+                        map.updateTile(Point2D(x, y), ShipFloorTile.TRAVERSABLE)
                     }
 
                     //Increment Relevant Ordinate
-                    when (direction) {
-                        Direction.NORTH -> y++
-                        Direction.EAST -> x++
-                        Direction.SOUTH -> y--
-                        Direction.WEST -> x--
-                    }
+                    move(direction)
 
                     //Updated Visited Tiles (Unless Backtracking)
-                    if (!shipFloorMap.hasRecorded(Point2D(x, y))) {
-                        visited.push(Pair(Point2D(x, y), direction))
+                    if (!map.hasRecorded(Point2D(x, y))) {
+                        path.push(Pair(Point2D(x, y), direction))
                     }
 
                     //Record New Coordinate As Droid Current Position
-                    shipFloorMap.updateTile(Point2D(x, y), ShipFloorTile.DROID)
+                    map.updateTile(Point2D(x, y), ShipFloorTile.DROID)
                 }
-                DroidResponse.LOCATED_OXYGEN_SYSTEM ->  {
+                DroidResponse.LOCATED_OXYGEN_SYSTEM -> {
                     //Record Current Coordinate As Traversable
-                    shipFloorMap.updateTile(Point2D(x, y), ShipFloorTile.TRAVERSABLE)
+                    map.updateTile(Point2D(x, y), ShipFloorTile.TRAVERSABLE)
 
                     //Increment Relevant Ordinate
-                    when (direction) {
-                        Direction.NORTH -> y++
-                        Direction.EAST -> x++
-                        Direction.SOUTH -> y--
-                        Direction.WEST -> x--
-                    }
+                    move(direction)
 
                     //Log Oxygen System Details
-                    oxygenSystemInfo = Pair(Point2D(x, y), visited.size)
-                    shipFloorMap.updateTile(Point2D(x, y), ShipFloorTile.OXYGEN_SYSTEM)
+                    oxygenSystemInfo = Pair(Point2D(x, y), path.size)
+                    map.updateTile(Point2D(x, y), ShipFloorTile.OXYGEN_SYSTEM)
 
                     //Log Oxygen System Visit
-                    visited.push(Pair(Point2D(x, y), direction))
+                    path.push(Pair(Point2D(x, y), direction))
                 }
             }
-
-            println(shipFloorMap)
         }
 
-        println(shipFloorMap)
+        println(map)
         println("Oxygen System Location ${oxygenSystemInfo!!.first}")
         return oxygenSystemInfo
     }
 
-    fun downloadShipMappingData() = shipFloorMap
+    private fun move(direction: Direction) {
+        when (direction) {
+            Direction.NORTH -> y++
+            Direction.EAST -> x++
+            Direction.SOUTH -> y--
+            Direction.WEST -> x--
+        }
+    }
 
-    private fun getNextDirection() = getUnexploredSurroundingCoordinateDirection() ?: visited.pop().second.reverse()
+    private fun getNextDirection() = getUnexploredSurroundingCoordinateDirection() ?: path.pop().second.reverse()
 
     private fun getUnexploredSurroundingCoordinateDirection(): Direction? {
-        if (!shipFloorMap.hasRecorded(Point2D(x, y + 1))) return Direction.NORTH
-        if (!shipFloorMap.hasRecorded(Point2D(x + 1, y))) return Direction.EAST
-        if (!shipFloorMap.hasRecorded(Point2D(x, y - 1))) return Direction.SOUTH
-        if (!shipFloorMap.hasRecorded(Point2D(x - 1, y))) return Direction.WEST
+        if (!map.hasRecorded(Point2D(x, y + 1))) return Direction.NORTH
+        if (!map.hasRecorded(Point2D(x + 1, y))) return Direction.EAST
+        if (!map.hasRecorded(Point2D(x, y - 1))) return Direction.SOUTH
+        if (!map.hasRecorded(Point2D(x - 1, y))) return Direction.WEST
         return null
     }
 }

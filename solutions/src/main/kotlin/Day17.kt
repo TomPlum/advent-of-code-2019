8e0f89f92f709dcfcef2dfdf599a160c0b790ec4
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-03-29 21:39:45 +0100

    Day 17 | Completed Part 1

--- solutions/src/main/kotlin/Day17.kt
+++ solutions/src/main/kotlin/Day17.kt
@@ -1,2 +1,12 @@
 package com.aoc.solutions
 
+import com.aoc.intcode.vacuum.VacuumRobot
+import input.Day
+import input.InputReader
+
+fun main() {
+    val instruction = InputReader().readInputAsSingleString(Day.from(17))
+    val scaffoldMap = VacuumRobot(instruction).exploreShipsExteriorScaffolding()
+    println("Solution Part 1: ${scaffoldMap.calculateAlignmentParameterSum()}")
+    println(scaffoldMap)
+}

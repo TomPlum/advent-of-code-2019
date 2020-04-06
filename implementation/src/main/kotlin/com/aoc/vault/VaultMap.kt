9d1e719b9e7034a6b08739f8fc66f6a9ba6078b6
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-04-06 19:45:21 +0100

    Day 18 | Added Example VaultMaps & Wrote Unit Tets

--- implementation/src/main/kotlin/com/aoc/vault/VaultMap.kt
+++ implementation/src/main/kotlin/com/aoc/vault/VaultMap.kt
@@ -5,7 +5,10 @@ import math.Point2D
 
 class VaultMap(private val initialData: List<String>) : Map<VaultTile>() {
 
+    private val keysCollected = mutableSetOf<VaultTile>()
+
     init {
+        //TODO: Wasn't this same thing done somewhere else? Can you move it to the common Map<T> class?
         var x = 0
         var y = 0
         initialData.forEach { row ->
@@ -15,6 +18,15 @@ class VaultMap(private val initialData: List<String>) : Map<VaultTile>() {
             }
             y++
         }
+    }
+
+    /**
+     * Explores the [VaultMap] by finding the keys to unlock the doors until all keys have been collected.
+     * The algorithm finds the order in which picking the keys up results in the lowest number of steps taken.
+     * @return The number of steps taken in order to collect all keys.
+     */
+    fun collectKeys(): Int {
+        return 0
     }
 
 }
---
+++ implementation/src/test/java/com/aoc/vault/VaultMapTest.kt
@@ -0,0 +1,45 @@
+package com.aoc.vault
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import input.InputReader
+import org.junit.jupiter.api.Test
+
+class VaultMapTest {
+
+    @Test
+    fun exampleOne() {
+        val input = InputReader().readInputAsString("/vault/example-1.txt").values
+        val steps = VaultMap(input).collectKeys()
+        assertThat(steps).isEqualTo(8)
+    }
+
+    @Test
+    fun exampleTwo() {
+        val input = InputReader().readInputAsString("/vault/example-2.txt").values
+        val steps = VaultMap(input).collectKeys()
+        assertThat(steps).isEqualTo(86)
+    }
+
+    @Test
+    fun exampleThree() {
+        val input = InputReader().readInputAsString("/vault/example-3.txt").values
+        val steps = VaultMap(input).collectKeys()
+        assertThat(steps).isEqualTo(132)
+    }
+
+    @Test
+    fun exampleFour() {
+        val input = InputReader().readInputAsString("/vault/example-4.txt").values
+        val steps = VaultMap(input).collectKeys()
+        assertThat(steps).isEqualTo(136)
+    }
+
+    @Test
+    fun exampleFive() {
+        val input = InputReader().readInputAsString("/vault/example-5.txt").values
+        val steps = VaultMap(input).collectKeys()
+        assertThat(steps).isEqualTo(81)
+    }
+
+}

b6982564e2fdcda31208482bd8ed4608e78120dd
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-05-29 23:44:09 +0100

    Day 20 | Added Portal Kotlin Doc

--- implementation/src/main/kotlin/com/aoc/maze/donut/portal/Portal.kt
+++ implementation/src/main/kotlin/com/aoc/maze/donut/portal/Portal.kt
@@ -2,8 +2,15 @@ package com.aoc.maze.donut.portal
 
 import com.aoc.math.Point2D
 import java.lang.IllegalArgumentException
+import com.aoc.maze.donut.PlutonianMaze
 
 //TODO: are 2 warp codes needed? A pair of warp-games IS a portal with one common warp code
+/**
+ * A space-time folding warp-gate in a [PlutonianMaze].
+ *
+ * @param warpCodes The two tiles in the [PlutonianMaze] that represent the portals name. E.g 'FG'
+ * @param entrances The two tiles in the [PlutonianMaze] that the portal teleports between.
+ */
 data class Portal(private val warpCodes: Pair<WarpCode, WarpCode>, private val entrances: Pair<Point2D, Point2D>) {
 
     fun hasEntrance(pos: Point2D): Boolean = entrances.first == pos || entrances.second == pos

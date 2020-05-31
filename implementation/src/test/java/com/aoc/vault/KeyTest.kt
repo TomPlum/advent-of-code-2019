2b9f50f400895fcb98a62aff04895e8f8aee8ecc
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-05-31 22:00:04 +0100

    Day 18 | Key Test - Added Missing Import

--- implementation/src/test/java/com/aoc/vault/KeyTest.kt
+++ implementation/src/test/java/com/aoc/vault/KeyTest.kt
@@ -4,6 +4,7 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
 import assertk.assertions.isNotEqualTo
+import assertk.assertions.isTrue
 import com.aoc.math.Point2D
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test

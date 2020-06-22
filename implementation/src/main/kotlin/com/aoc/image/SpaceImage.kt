892b3cfe89bd8b79175743bb2c8c89a63c5e2ad0
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-06-22 16:26:17 +0100

    Day 8 | Test Coverage - Increased -> 100%

--- implementation/src/main/kotlin/com/aoc/image/SpaceImage.kt
+++ implementation/src/main/kotlin/com/aoc/image/SpaceImage.kt
@@ -2,8 +2,8 @@ package com.aoc.image
 
 class SpaceImage(val layer: SpaceImageLayer) {
 
-    override fun toString(): String = layer.rows.joinToString(separator = "\n") { "$it" }
-
     fun toRaw(): String = layer.rows.joinToString(separator = "\n") { it.toRaw() }
+
+    override fun toString(): String = layer.rows.joinToString(separator = "\n") { "$it" }
 
 }
--- implementation/src/test/kotlin/com/aoc/image/SpaceImageTest.kt
+++ implementation/src/test/kotlin/com/aoc/image/SpaceImageTest.kt
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test
 
 class SpaceImageTest {
     @Test
-    fun rawToStringTest() {
+    fun toRawString() {
         val pixels = listOf(Pixel.WHITE, Pixel.BLACK, Pixel.WHITE, Pixel.BLACK)
         val image = SpaceImage(SpaceImageLayer(pixels, SpaceImageDimensions(2, 2)))
         assertThat(image.toRaw()).isEqualTo("1 0\n1 0")
+    }
+
+    @Test
+    fun toStringTest() {
+        val pixels = listOf(Pixel.WHITE, Pixel.BLACK, Pixel.WHITE, Pixel.BLACK)
+        val image = SpaceImage(SpaceImageLayer(pixels, SpaceImageDimensions(2, 2)))
+        assertThat(image.toString()).isEqualTo("#  \n#  ")
     }
 }

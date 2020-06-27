52326f08b46eede137dd9102c7cfc4655b260e5f
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-06-27 09:07:29 +0100

    Day 22 | Refactored SpaceCardDeckShuffler (Language / Readability)

--- implementation/src/test/kotlin/com/aoc/cards/SpaceCardDeckShufflerTest.kt
+++ implementation/src/test/kotlin/com/aoc/cards/SpaceCardDeckShufflerTest.kt
@@ -11,32 +11,32 @@ class SpaceCardDeckShufflerTest {
     @Test
     fun exampleOne() {
         val input = InputReader().readInputAsString("/cards/example-instructions-1.txt")
-        val shuffler = SpaceCardDeckShuffler(SpaceCardDeckFactory.factoryOrder(10))
-        val shuffledDeck = shuffler.shuffle(ShuffleInstructionParser.parse(input.values))
+        val shuffler = SpaceCardDeckShuffler(ShuffleInstructionParser.parse(input.values))
+        val shuffledDeck = shuffler.shuffle(SpaceCardDeckFactory.factoryOrder(10))
         assertThat(shuffledDeck).isEqualTo(deckWithCards(0,3,6,9,2,5,8,1,4,7))
     }
 
     @Test
     fun exampleTwo() {
         val input = InputReader().readInputAsString("/cards/example-instructions-2.txt")
-        val shuffler = SpaceCardDeckShuffler(SpaceCardDeckFactory.factoryOrder(10))
-        val shuffledDeck = shuffler.shuffle(ShuffleInstructionParser.parse(input.values))
+        val shuffler = SpaceCardDeckShuffler(ShuffleInstructionParser.parse(input.values))
+        val shuffledDeck = shuffler.shuffle(SpaceCardDeckFactory.factoryOrder(10))
         assertThat(shuffledDeck).isEqualTo(deckWithCards(3,0,7,4,1,8,5,2,9,6))
     }
 
     @Test
     fun exampleThree() {
         val input = InputReader().readInputAsString("/cards/example-instructions-3.txt")
-        val shuffler = SpaceCardDeckShuffler(SpaceCardDeckFactory.factoryOrder(10))
-        val shuffledDeck = shuffler.shuffle(ShuffleInstructionParser.parse(input.values))
+        val shuffler = SpaceCardDeckShuffler(ShuffleInstructionParser.parse(input.values))
+        val shuffledDeck = shuffler.shuffle(SpaceCardDeckFactory.factoryOrder(10))
         assertThat(shuffledDeck).isEqualTo(deckWithCards(6,3,0,7,4,1,8,5,2,9))
     }
 
     @Test
     fun exampleFour() {
         val input = InputReader().readInputAsString("/cards/example-instructions-4.txt")
-        val shuffler = SpaceCardDeckShuffler(SpaceCardDeckFactory.factoryOrder(10))
-        val shuffledDeck = shuffler.shuffle(ShuffleInstructionParser.parse(input.values))
+        val shuffler = SpaceCardDeckShuffler(ShuffleInstructionParser.parse(input.values))
+        val shuffledDeck = shuffler.shuffle(SpaceCardDeckFactory.factoryOrder(10))
         assertThat(shuffledDeck).isEqualTo(deckWithCards(9,2,5,8,1,4,7,0,3,6))
     }
 
@@ -45,7 +45,7 @@ class SpaceCardDeckShufflerTest {
         val input = InputReader().readInputString(Day.from(22))
         val instructions = ShuffleInstructionParser.parse(input.values)
         val deck = SpaceCardDeckFactory.default()
-        val shuffler = SpaceCardDeckShuffler(deck)
-        assertThat(shuffler.shuffle(instructions).getCardWithValue(2019)).isEqualTo(2604)
+        val shuffler = SpaceCardDeckShuffler(instructions)
+        assertThat(shuffler.shuffle(deck).getCardWithValue(2019)).isEqualTo(2604)
     }
 }
--- solutions/src/main/kotlin/com/aoc/solutions/Day22.kt
+++ solutions/src/main/kotlin/com/aoc/solutions/Day22.kt
@@ -10,10 +10,9 @@ fun main() {
     val input = InputReader().readInputString(Day.from(22))
     val instructions = ShuffleInstructionParser.parse(input.values)
     val deck = SpaceCardDeckFactory.default()
-    val shuffler = SpaceCardDeckShuffler(deck)
-    println("Part 1 Solution: ${shuffler.shuffle(instructions).getCardWithValue(2019)}")
+    val shuffler = SpaceCardDeckShuffler(instructions)
+    println("Part 1 Solution: ${shuffler.shuffle(deck).getCardWithValue(2019)}")
 
     val giantDeck = SpaceCardDeckFactory.factoryOrder(119315717514047)
-    val giantShuffler = SpaceCardDeckShuffler(giantDeck)
-    println("Part 2 Solution: ${giantShuffler.shuffle(instructions, 101741582076661).getCard(2020).value}")
+    println("Part 2 Solution: ${shuffler.shuffle(giantDeck, 101741582076661).getCard(2020).value}")
 }

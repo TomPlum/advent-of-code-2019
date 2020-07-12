f8d3c45b9eeb4de1b5834bfe7a6217073d57c1f1
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-07-12 12:36:14 +0100

    Day 23 | Part 2 - Added NAT Network Status Tests

--- implementation/src/test/kotlin/com/aoc/intcode/network/NATInterceptorTest.kt
+++ implementation/src/test/kotlin/com/aoc/intcode/network/NATInterceptorTest.kt
@@ -2,6 +2,7 @@ package com.aoc.intcode.network
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import com.aoc.intcode.network.packet.Packet
 import com.aoc.intcode.network.packet.PacketData
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -27,5 +28,29 @@ class NATInterceptorTest {
     fun transmitWithoutHavingReceivedAnyPacketData() {
         val e = assertThrows<IllegalStateException> { NATInterceptor().transmit() }
         assertThat(e.message).isEqualTo("NAT has not received any packets")
+    }
+
+    @Test
+    fun checkNetworkStatusWhenIdle() {
+        val computer = getIdleComputer()
+        val nat = NATInterceptor()
+        assertThat(nat.checkNetworkStatus(listOf(computer, computer, computer))).isEqualTo(NetworkStatus.IDLE)
+    }
+
+    @Test
+    fun checkNetworkStatusWhenActive() {
+        val nat = NATInterceptor()
+        val network = listOf(getIdleComputer(), getIdleComputer(), getActiveComputer(), getIdleComputer())
+        assertThat(nat.checkNetworkStatus(network)).isEqualTo(NetworkStatus.ACTIVE)
+    }
+
+    private fun getActiveComputer(): NetworkComputer {
+        val active = NetworkComputer("99")
+        active.listen(Packet(NetworkAddress(50), PacketData(12, 57)))
+        return active
+    }
+
+    private fun getIdleComputer(): NetworkComputer {
+        return NetworkComputer("99")
     }
 }

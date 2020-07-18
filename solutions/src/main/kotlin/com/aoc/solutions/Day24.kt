4a00b4f3fa15bb677fddfcec8bb1a86667ca2635
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-07-18 21:55:59 +0100

    Day 24 | Part 2 - Added Solution

--- solutions/src/main/kotlin/com/aoc/solutions/Day24.kt
+++ solutions/src/main/kotlin/com/aoc/solutions/Day24.kt
@@ -2,11 +2,17 @@ package com.aoc.solutions
 
 import com.aoc.input.Day
 import com.aoc.input.InputReader
-import com.aoc.monitoring.eris.ErisLayoutMonitor
+import com.aoc.monitoring.eris.PlanarErisLayoutMonitor
 import com.aoc.monitoring.eris.PlanarErisPlanetLayout
+import com.aoc.monitoring.eris.RecursiveErisLayoutMonitor
+import com.aoc.monitoring.eris.RecursiveErisPlanetLayout
 
 fun main() {
-    val scanData = InputReader().readInputString(Day.from(24))
-    val monitor = ErisLayoutMonitor(PlanarErisPlanetLayout(scanData.values))
-    println("Solution Part 1: ${monitor.watchForRepeatingLayout().getBioDiversityRating()}")
+    val scanData = InputReader().readInputString(Day.from(24)).values
+
+    val planarMonitor = PlanarErisLayoutMonitor(PlanarErisPlanetLayout(scanData))
+    println("Solution Part 1: ${planarMonitor.watchForRepeatingLayout().getBioDiversityRating()}")
+
+    val recursiveMonitor = RecursiveErisLayoutMonitor(RecursiveErisPlanetLayout(scanData))
+    println("Solution Part 2: ${recursiveMonitor.watch(200)} bugs")
 }

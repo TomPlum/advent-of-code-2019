c65c91f90508dbd03b6b80062a96da42416686b7
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-07-20 21:12:08 +0100

    Day 1 | Added FuelCalculator Kotlin Doc (#3)

--- implementation/src/main/kotlin/com/aoc/fuel/calculator/FuelCalculator.kt
+++ implementation/src/main/kotlin/com/aoc/fuel/calculator/FuelCalculator.kt
@@ -2,13 +2,37 @@ package com.aoc.fuel.calculator
 
 import kotlin.math.floor
 
+/**
+ * Part of the launch preparations to save Santa requires the ship to be fueled.
+ *
+ * At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper.
+ * They haven't determined the amount of fuel required yet.
+ */
 class FuelCalculator {
+    /**
+     * Fuel required to launch a given module is based on its [mass].
+     * Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+     * @return fuel quantity
+     */
     fun calculateModuleFuel(mass: Int) : Int = (floor(mass.toDouble() / 3) - 2).toInt()
 
-    fun calculateTotalFuelForModule(fuel: List<Int>) : Int = fuel.map { e -> calculateModuleFuel(e) }.sum()
+    /**
+     * Calculates the sum of the [fuel] requirements for all of the modules on the spacecraft.
+     */
+    fun calculateTotalFuelForModule(fuel: List<Int>) : Int = fuel.sumBy { calculateModuleFuel(it) }
 
-    fun calculateTotalAdditionalFuelForModule(fuel: List<Int>) : Int = fuel.map { e -> calculateAdditionalFuel(e) }.sum()
+    /**
+     * Calculates the sum of the [fuel] requirements for all of the modules on the spacecraft when also taking into
+     * account the mass of the added fuel.
+     */
+    fun calculateTotalAdditionalFuelForModule(fuel: List<Int>) : Int = fuel.sumBy { calculateAdditionalFuel(it) }
 
+    /**
+     * Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2.
+     * However, that fuel also requires fuel, and that fuel requires fuel, and so on.
+     * Any mass that would require negative fuel should instead be treated as if it requires zero fuel.
+     * @return additional fuel quantity
+     */
     fun calculateAdditionalFuel(mass: Int) : Int {
         var currentMass = mass
         var additionalFuel = 0

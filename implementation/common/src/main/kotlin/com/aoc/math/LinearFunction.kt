2b72e61ad49b3bad4720f7dbe516c2b2e0b327ce
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-07-25 22:14:58 +0100

    Common | Added Linear Function Unit Tests

--- implementation/common/src/main/kotlin/com/aoc/math/LinearFunction.kt
+++ implementation/common/src/main/kotlin/com/aoc/math/LinearFunction.kt
@@ -3,10 +3,10 @@ package com.aoc.math
 import java.math.BigInteger
 
 /**
- * f(x) = ax + b
+ * Represents a linear function in the form; f(x) = ax + b
  */
 data class LinearFunction(val k: BigInteger, val m: BigInteger) {
-    fun apply(x: BigInteger): BigInteger = x.multiply(k).add(m)
+    fun apply(x: BigInteger): BigInteger = k.multiply(x).add(m)
 
     fun compose(f: LinearFunction) = LinearFunction(k.times(f.k), k.times(f.m).plus(m))
 }
---
+++ implementation/common/src/test/kotlin/com/aoc/math/LinearFunctionTest.kt
@@ -0,0 +1,63 @@
+package com.aoc.math
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotEqualTo
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class LinearFunctionTest {
+    @Nested
+    inner class Apply {
+        @Test
+        fun positiveIntegers() {
+            assertThat(LinearFunction(2.big(), 4.big()).apply(5.big())).isEqualTo(14.big())
+        }
+
+        @Test
+        fun withNegativeIntegers() {
+            assertThat(LinearFunction((-6).big(), 4.big()).apply((-2).big())).isEqualTo(16.big())
+        }
+    }
+
+    @Nested
+    inner class Compose {
+        @Test
+        fun compose() {
+            val f = LinearFunction(2.big(), 4.big())
+            val k = LinearFunction((-6).big(), 4.big())
+            assertThat(f.compose(k)).isEqualTo(LinearFunction((-12).big(), 12.big()))
+        }
+    }
+
+    @Nested
+    inner class Equality {
+        @Test
+        fun equal() {
+            val first = LinearFunction(12.big(), 26.big())
+            val second = LinearFunction(12.big(), 26.big())
+            assertThat(first).isEqualTo(second)
+        }
+
+        @Test
+        fun notEqualDifferentFirstTerm() {
+            val first = LinearFunction(15.big(), 26.big())
+            val second = LinearFunction(12.big(), 26.big())
+            assertThat(first).isNotEqualTo(second)
+        }
+
+        @Test
+        fun notEqualDifferentSecondTerm() {
+            val first = LinearFunction(12.big(), 26.big())
+            val second = LinearFunction(12.big(), 16.big())
+            assertThat(first).isNotEqualTo(second)
+        }
+    }
+
+    @Test
+    fun toStringTest() {
+        assertThat(LinearFunction((-6).big(), 4.big()).toString()).isEqualTo("LinearFunction(k=-6, m=4)")
+    }
+
+    private fun Int.big() = this.toBigInteger()
+}

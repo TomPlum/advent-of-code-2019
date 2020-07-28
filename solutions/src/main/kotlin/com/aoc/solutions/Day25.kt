49cffc341e11f363ca54b46ad75ab4323bc7e163
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-07-28 22:15:26 +0100

    Day 25 | Added Puzzle Input

---
+++ solutions/src/main/kotlin/com/aoc/solutions/Day25.kt
@@ -0,0 +1,11 @@
+package com.aoc.solutions
+
+import com.aoc.Day
+import com.aoc.input.InputReader
+import com.aoc.intcode.droid.cryo.CommandRuntime
+
+fun main() {
+    val input = InputReader.read<String>(Day(25)).asSingleString()
+    val runtime = CommandRuntime(input)
+    runtime.run()
+}

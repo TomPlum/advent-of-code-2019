1181dbe7e4803d4bf76af8ce561724c43d1d8679
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-07-29 21:55:22 +0100

    Common | Added StringExtensions 'Capitalise Words' Function (#13)

--- implementation/common/src/main/kotlin/com/aoc/extensions/StringExtensions.kt
+++ implementation/common/src/main/kotlin/com/aoc/extensions/StringExtensions.kt
@@ -1,3 +1,4 @@
 package com.aoc.extensions
 
 fun String.toAscii(): List<Long> = this.map { it.toLong() }
+fun String.capitaliseWords(): String = this.split(" ").joinToString(" ") { it.capitalize() }
--- implementation/common/src/test/kotlin/com/aoc/extensions/StringExtensionsKtTest.kt
+++ implementation/common/src/test/kotlin/com/aoc/extensions/StringExtensionsKtTest.kt
@@ -28,4 +28,32 @@ class StringExtensionsTest {
             assertThat("Hello123".toAscii()).isEqualTo(listOf<Long>(72,101,108,108,111,49,50,51))
         }
     }
+
+    @Nested
+    inner class CapitaliseWords {
+        @Test
+        fun singleWordLowercase() {
+            assertThat("hello".capitaliseWords()).isEqualTo("Hello")
+        }
+
+        @Test
+        fun singleWordUppercaseFirst() {
+            assertThat("Hello".capitaliseWords()).isEqualTo("Hello")
+        }
+
+        @Test
+        fun singleWordUppercase() {
+            assertThat("HELLO".capitaliseWords()).isEqualTo("HELLO")
+        }
+
+        @Test
+        fun emptyString() {
+            assertThat("".capitaliseWords()).isEqualTo("")
+        }
+
+        @Test
+        fun twoWordsLowercase() {
+            assertThat("hello there".capitaliseWords()).isEqualTo("Hello There")
+        }
+    }
 }

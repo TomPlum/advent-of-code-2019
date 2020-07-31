26b5502d6f23833637c8e1903106b8b6ef7de94c
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-07-31 17:21:27 +0100

    Day 25 | StarShip Map Initial Implementation (Creates ASCII Map)

--- implementation/src/main/kotlin/com/aoc/intcode/droid/cryo/StarShipMap.kt
+++ implementation/src/main/kotlin/com/aoc/intcode/droid/cryo/StarShipMap.kt
@@ -1,14 +1,197 @@
 package com.aoc.intcode.droid.cryo
 
+import com.aoc.intcode.droid.cryo.StarShipMap.CornerGlyph.*
+import com.aoc.intcode.droid.cryo.StarShipMap.EdgeGlyph.*
 import com.aoc.map.AdventMap2D
+import com.aoc.math.Direction
+import com.aoc.math.Direction.*
 import com.aoc.math.Point2D
 
 class StarShipMap : AdventMap2D<Room>() {
-    fun addRoom(position: Point2D, room: Room) {
-        addTile(position, room)
-    }
+
+    var droidPosition = Point2D(0, 0)
+
+    fun addRoom(position: Point2D, room: Room) = addTile(position, room)
+
+    fun getRoom(position: Point2D) = getTile(position)
 
     fun rooms() = tileQuantity()
 
-    fun print(): String = ""
+    fun display(): String {
+        return (yMax() downTo yMin()).joinToString("\n") { y ->
+            val roomString = StringBuilder()
+
+            val xBreath = xMin()..xMax()
+
+            xBreath.forEach { x -> roomString.append(getTopLayer(x, y)) }
+            roomString.append("\n")
+
+            xBreath.forEach { x -> roomString.append(getNameLayer(x, y)) }
+            roomString.append("\n")
+
+            xBreath.forEach { x -> roomString.append(getHorizontalCorridorLayer(x, y)) }
+            roomString.append("\n")
+
+            xBreath.forEach { x -> roomString.append(getFirstItemLayer(x, y)) }
+
+            //Only append the bottom layer if we're drawing the bottom layer of rooms on the map
+            if (y == yMin()) {
+                roomString.append("\n")
+                xBreath.forEach { x -> roomString.append(getBottomLayer(x, y)) }
+            }
+
+            roomString
+        }
+    }
+
+    private fun Point2D.hasRoom(direction: Direction) = hasRecorded(this.shift(direction))
+
+    private fun getTopLayer(x: Int, y: Int): String {
+        val room = getTile(Point2D(x, y), Room.empty())
+        return getTopLeftCorner(Point2D(x, y)).toString() + getTop(room) + getTopRightCorner(Point2D(x, y))
+    }
+
+    private fun getBottomLayer(x: Int, y: Int): String {
+        val pos = Point2D(x, y)
+        val room = getTile(pos, Room.empty())
+        return getBottomLeftCorner(pos).toString() + getBottom(room) + getBottomRightCorner(pos)
+    }
+
+    private fun getNameLayer(x: Int, y: Int): String {
+        val room = getTile(Point2D(x, y), Room.empty())
+
+        val layer = StringBuilder(VERTICAL.toString())
+        val nameLength = room.name.length
+        if (nameLength <= 12) {
+            val leftPadding = (12 - nameLength) / 2
+            val rightPadding = 12 - leftPadding - nameLength
+            layer.append(" ".repeat(leftPadding)).append(room.name).append(" ".repeat(rightPadding))
+        } else {
+            layer.append(room.name.substring(0, 9)).append("...")
+        }
+        layer.append(VERTICAL)
+        return layer.toString()
+    }
+
+    private fun getHorizontalCorridorLayer(x: Int, y: Int): String {
+        val room = getTile(Point2D(x, y), Room.empty())
+
+        val layer = StringBuilder()
+        when {
+            room.doors.contains(LEFT) -> layer.append(VERTICAL_DOOR)
+            else -> layer.append(VERTICAL)
+        }
+
+        if (Point2D(x, y) == droidPosition) {
+            layer.append(" ".repeat(5)).append(MiscGlyphs.DROID).append(" ".repeat(5))
+        } else {
+            layer.append(" ".repeat(12))
+        }
+
+        when {
+            room.doors.contains(RIGHT) -> layer.append(VERTICAL_DOOR)
+            else -> layer.append(VERTICAL)
+        }
+
+        return layer.toString()
+    }
+
+    private fun getFirstItemLayer(x: Int, y: Int): String {
+        val room = getTile(Point2D(x, y), Room.empty())
+
+        val layer = StringBuilder(VERTICAL.toString())
+        if (room.hasItems()) {
+            val item = room.items.first()
+            val itemNameLength = item.name.length
+            if (itemNameLength <= 12) {
+                val leftPadding = (12 - itemNameLength) / 2
+                val rightPadding = 12 - leftPadding - itemNameLength
+                layer.append(" ".repeat(leftPadding)).append(item.name).append(" ".repeat(rightPadding))
+            } else {
+                layer.append(item.name.substring(0, 9)).append("...")
+            }
+        } else {
+            layer.append(" ".repeat(12))
+        }
+        layer.append(VERTICAL)
+        return layer.toString()
+    }
+
+
+    private fun getTopLeftCorner(pos: Point2D): CornerGlyph = when {
+        pos.hasRoom(UP) && pos.hasRoom(LEFT) && pos.shift(LEFT).hasRoom(UP) -> CROSS_SECTION
+        pos.hasRoom(UP) && pos.hasRoom(LEFT) -> TRIPLE_LEFT
+        pos.hasRoom(UP) -> TRIPLE_RIGHT
+        pos.hasRoom(LEFT) -> TRIPLE_DOWN
+        else -> TOP_LEFT_CORNER
+    }
+
+    private fun getTopRightCorner(pos: Point2D): CornerGlyph = when {
+        pos.hasRoom(UP) && pos.hasRoom(RIGHT) && pos.shift(RIGHT).hasRoom(UP) -> CROSS_SECTION
+        pos.hasRoom(UP) && pos.hasRoom(RIGHT) -> TRIPLE_RIGHT
+        pos.hasRoom(UP) -> TRIPLE_LEFT
+        pos.hasRoom(RIGHT) -> TRIPLE_DOWN
+        else -> TOP_RIGHT_CORNER
+    }
+
+    private fun getBottomLeftCorner(pos: Point2D): CornerGlyph = when {
+        pos.hasRoom(DOWN) && pos.hasRoom(LEFT) && pos.shift(LEFT).hasRoom(DOWN) -> CROSS_SECTION
+        pos.hasRoom(DOWN) && pos.hasRoom(LEFT) -> TRIPLE_LEFT
+        pos.hasRoom(DOWN) -> TRIPLE_RIGHT
+        pos.hasRoom(LEFT) -> TRIPLE_UP
+        else -> BOTTOM_LEFT_CORNER
+    }
+
+    private fun getBottomRightCorner(pos: Point2D): CornerGlyph = when {
+        pos.hasRoom(DOWN) && pos.hasRoom(RIGHT) && pos.shift(RIGHT).hasRoom(DOWN) -> CROSS_SECTION
+        pos.hasRoom(DOWN) && pos.hasRoom(RIGHT) -> CROSS_SECTION
+        pos.hasRoom(DOWN) -> TRIPLE_LEFT
+        pos.hasRoom(RIGHT) -> TRIPLE_UP
+        else -> BOTTOM_RIGHT_CORNER
+    }
+
+    private fun getTop(room: Room): String {
+        if (room.doors.contains(UP)) {
+            val edge = HORIZONTAL.toString().repeat(4)
+            return edge + HORIZONTAL_DOOR + edge
+        }
+        return HORIZONTAL.toString().repeat(12)
+    }
+
+    private fun getBottom(room: Room): String {
+        if (room.doors.contains(DOWN)) {
+            val edge = HORIZONTAL.toString().repeat(4)
+            return edge + HORIZONTAL_DOOR + edge
+        }
+        return HORIZONTAL.toString().repeat(12)
+    }
+
+    private enum class MiscGlyphs(val glyph: String) {
+        DROID("/\\");
+
+        override fun toString(): String = glyph
+    }
+
+    private enum class CornerGlyph(val glyph: String) {
+        CROSS_SECTION("┼"),
+        TRIPLE_LEFT("┤"),
+        TRIPLE_RIGHT("├"),
+        TRIPLE_DOWN("┬"),
+        TRIPLE_UP("┴"),
+        TOP_LEFT_CORNER("┌"),
+        TOP_RIGHT_CORNER("┐"),
+        BOTTOM_LEFT_CORNER("└"),
+        BOTTOM_RIGHT_CORNER("┘");
+
+        override fun toString(): String = glyph
+    }
+
+    private enum class EdgeGlyph(val glyph: String) {
+        HORIZONTAL("─"),
+        VERTICAL("│"),
+        HORIZONTAL_DOOR("    "),
+        VERTICAL_DOOR(" ");
+
+        override fun toString(): String = glyph
+    }
 }
--- implementation/src/test/kotlin/com/aoc/intcode/droid/cryo/StarShipMapTest.kt
+++ implementation/src/test/kotlin/com/aoc/intcode/droid/cryo/StarShipMapTest.kt
@@ -2,7 +2,7 @@ package com.aoc.intcode.droid.cryo
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import com.aoc.math.Direction
+import com.aoc.log.AdventLogger
 import com.aoc.math.Direction.*
 import com.aoc.math.Point2D
 import org.junit.jupiter.api.Nested
@@ -20,12 +20,35 @@ class StarShipMapTest {
     }
 
     @Nested
-    inner class Print {
+    inner class Display {
         @Test
         fun singleRoom() {
             val map = StarShipMap()
             map.addRoom(Point2D(0, 0), Room("Kitchen", listOf(UP, RIGHT, LEFT), mutableListOf(Item("knife"))))
-            assertThat(map.print()).isEqualTo("")
+            AdventLogger.info(map.display())
+            assertThat(map.display()).isEqualTo(
+                    "┌────    ────┐\n" +
+                    "│  Kitchen   │\n" +
+                    "              \n" +
+                    "│   knife    │\n" +
+                    "└────────────┘"
+            )
+        }
+
+        @Test
+        fun twoRoomsVerticallyAdjacent() {
+            val map = StarShipMap()
+            map.addRoom(Point2D(0, 0), Room("Kitchen", listOf(UP, RIGHT, LEFT), mutableListOf(Item("knife"))))
+            map.addRoom(Point2D(0, 1), Room("Hull", listOf(DOWN, RIGHT, LEFT), mutableListOf()))
+            AdventLogger.info(map.display())
+        }
+
+        @Test
+        fun twoRoomsHorizontallyAdjacent() {
+            val map = StarShipMap()
+            map.addRoom(Point2D(0, 0), Room("Kitchen", listOf(UP, RIGHT, LEFT), mutableListOf(Item("knife"))))
+            map.addRoom(Point2D(1, 0), Room("Hull", listOf(DOWN, RIGHT, LEFT), mutableListOf()))
+            AdventLogger.info(map.display())
         }
     }
 }

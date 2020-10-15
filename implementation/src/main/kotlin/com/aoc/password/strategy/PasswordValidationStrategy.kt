64d136447491256d66cc19b410d44f6c2957f335
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-10-15 11:10:48 +0100

    Day 4 | Added Password Validation Stategy Interface Kotlin Doc

--- implementation/src/main/kotlin/com/aoc/password/strategy/PasswordValidationStrategy.kt
+++ implementation/src/main/kotlin/com/aoc/password/strategy/PasswordValidationStrategy.kt
@@ -2,6 +2,11 @@ package com.aoc.password.strategy
 
 import com.aoc.password.Password
 
+/**
+ * A strategy for validating a [Password].
+ * @see InitialValidation
+ * @see RevisedValidation
+ */
 interface PasswordValidationStrategy {
     fun validate(password: Password): Boolean
 }
